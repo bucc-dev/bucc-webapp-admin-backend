90bf6fadff876fab3cfba104f82bc7abccfbec82
tami-cp0 <findtamilore>
2024-11-07 23:26:52 +0100

    Add: error middleware

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,15 @@
 import { Response, Request, NextFunction, Router } from "express";
+import { handleError, CustomError } from "../middleware/errorHandler";
 import userRouter from "./user";
 
 
 const router: Router = Router();
 
 router.use('/api/v1/users', userRouter);
+
+router.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+    handleError(err, res);
+});
 
 router.all('*', (req: Request, res: Response) => {
     res.status(404).json({
