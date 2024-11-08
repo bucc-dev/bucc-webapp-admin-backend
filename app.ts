f9cbf3b3437dbeea88295ca8adfd8dbf93d558a4
tami-cp0 <findtamilore>
2024-11-08 21:51:19 +0100

    wip: restructure

--- app.ts
+++ app.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from "dotenv";
-import { CustomError } from "./middleware/errorHandler";
-import { Response, Request } from "express";
-import { handleError } from "./middleware/errorHandler";
 import router from "./routes";
 
 config();
 
 const app = express();
 
-const port = Number(process.env.PORT) || 3000;
-const host = '0.0.0.0';
+const port: number = Number(process.env.PORT) || 3000;
+const host: string = '0.0.0.0';
 
 const allowedOrigins: string[] = [host];
 app.use(cors({
