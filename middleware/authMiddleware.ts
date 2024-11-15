3f3c1ec2b93f200127dc630bc47430805d85a76f
tami-cp0 <findtamilore>
2024-11-15 14:36:06 +0100

    Add: refresh token logic

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,21 +1,46 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { decode, TokenExpiredError } from 'jsonwebtoken';
 import { CustomJwtPayload } from '../interfaces';
 import { ErrorHandler } from './errorHandler';
+import User from '../models/users';
+import IUser from '../interfaces/user';
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const token: string | undefined = req.headers.authorization?.split(' ')[1];
+
+const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const accessToken: string | undefined = req.cookies?.accessToken;
+    const refreshToken: string | undefined = req.cookies?.refreshToken;
+    const secret: string | undefined = process.env.JWT_SECRET;
+
+    if (!accessToken || !refreshToken || !secret) {
+        return next(new ErrorHandler(401, 'Login required'));
+    }
+
+    // ensure refreshtoken is also valid
+    const refreshIsValid = jwt?.verify(refreshToken, secret);
+    if (!refreshIsValid) return next(new ErrorHandler(401, 'Login required'));
 
     try {
-        const secret: string | undefined = process.env.JWT_SECRET;
-
-        if (!token || !secret) throw new ErrorHandler(401, 'Login required');
-
-        const decoded: CustomJwtPayload = jwt.verify(token, secret) as CustomJwtPayload;
+        const decoded: CustomJwtPayload = jwt?.verify(accessToken, secret) as CustomJwtPayload;
         req.user = decoded;
-        next();
+
+        return next();
     } catch (error) {
-        next(error);
+        if (error instanceof TokenExpiredError) {
+            const decoded = jwt.decode(accessToken) as CustomJwtPayload;
+
+            const user: IUser | null = await User.findById(decoded._id);
+
+            if (user && user.refreshToken === refreshToken) {
+                const payload: CustomJwtPayload = { _id: user._id, email: user.email, refreshed: true };
+                const newAccessToken: string = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '5min' });
+
+                res.cookie('accessToken', newAccessToken, { maxAge: 5 * 60 * 1000, httpOnly: true, secure: true }); // 5 mins
+
+                req.user = payload;
+                return next();
+            }
+        }
+        return next(new ErrorHandler(401, 'Login required'));
     }
 };
 
