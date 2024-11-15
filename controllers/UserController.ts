70113fc01953dfcbac928dd3d91a1a1efc123c9b
tami-cp0 <findtamilore>
2024-11-15 18:56:51 +0100

    Add: logout all devices function

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -25,13 +25,12 @@ class UserController {
             if (!user.isVerified) throw new ErrorHandler(403, 'Account is not verified');
 
 			if (user) {
-				const payload: CustomJwtPayload = { _id: user._id, email: user.email, refreshed: false };
+				const payload: CustomJwtPayload = { _id: user._id, email: user.email, accessLevel: user.accessLevel };
 				const accessToken: string = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '5min'});
-				const refreshToken: string = await user.generateRefreshToken();
+				const refreshToken: string = await user.generateRefreshToken(payload);
 
                 res.cookie('accessToken', accessToken, { maxAge: 5 * 60 * 1000, httpOnly: true, secure: true });
                 res.cookie('refreshToken', refreshToken, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true }); // 7 days in ms
-
 
                 res.status(200).json({
                     status: 'success',
@@ -49,13 +48,40 @@ class UserController {
 
 			if (!user) throw new ErrorHandler(401, 'Login required');
 
-			if (user.refreshToken) {
-				user.refreshToken = '';
+			const refreshToken: string = req.cookies?.refreshToken;
+
+			if (user.refreshTokens.includes(refreshToken)) {
+				user.refreshTokens = user.refreshTokens.filter(rt => rt !== refreshToken);
 				await user.save();
 			}
 
-			res.cookie('accessToken', '', { maxAge: 0, httpOnly: true, secure: true });
-            res.cookie('refreshToken', '', { maxAge: 0, httpOnly: true, secure: true });
+			res.clearCookie('accessToken', { httpOnly: true, secure: true });
+            res.clearCookie('refreshToken', { httpOnly: true, secure: true });
+
+			res.status(200).json({
+				status: 'success',
+				message: 'Logged out successfully',
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	static async logoutAllDevices(req: Request, res: Response, next: NextFunction) {
+		try {
+			const user: IUser | null = await User.findById(req.user._id);
+
+			if (!user) throw new ErrorHandler(401, 'Login required');
+
+			const refreshToken: string = req.cookies?.refreshToken;
+
+			if (user.refreshTokens && user.refreshTokens.length > 0) {
+				user.refreshTokens = [];
+				await user.save();
+			}
+
+			res.clearCookie('accessToken', { httpOnly: true, secure: true });
+            res.clearCookie('refreshToken', { httpOnly: true, secure: true });
 
 			res.status(200).json({
 				status: 'success',
