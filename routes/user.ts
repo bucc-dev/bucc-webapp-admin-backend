f7c441353d2cd396a1d9f638141b596b4d294acc
tami-cp0 <findtamilore>
2024-11-16 22:53:10 +0100

    Add: register route

--- routes/user.ts
+++ routes/user.ts
@@ -5,6 +5,7 @@ import { authLimiter, generalLimiter } from "../utils/limiters";
 
 const userRouter = Router();
 
+userRouter.route('/register').post(UserController.register);
 userRouter.route('/login').post(authLimiter, UserController.login);
 userRouter.route('/logout').post(authMiddleware, generalLimiter, UserController.logout);
 userRouter.route('/logout-all-devices').post(authMiddleware, generalLimiter, UserController.logoutAllDevices);
