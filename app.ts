02d7c776e0aeb8c31e3683be4b5c4b2eef2679ff
tami-cp0 <findtamilore>
2024-11-16 22:50:31 +0100

    Add: ping db and db connection

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { config } from "dotenv";
 import cookieParser from 'cookie-parser';
+import https from 'https';
+import mongoose from 'mongoose';
 import router from "./routes";
 
 config();
@@ -10,6 +12,13 @@ const app = express();
 
 const port: number = Number(process.env.PORT) || 3000;
 const host: string = '0.0.0.0';
+const serverURL: string = `http://localhost:${port}`;
+
+mongoose.connect(process.env.MONGODB_URI as string)
+    .then(() => console.log('MongoDB is connected'))
+    .catch(error => {
+        console.log(`Failed to connect to mongodb: ${error}`);
+    });
 
 const allowedOrigins: string[] = [host];
 app.use(cors({
@@ -26,3 +35,10 @@ app.use(router);
 app.listen(port, host, () => {
     console.log(`Listening on http://localhost:${port}`);
 });
+
+// to keep render alive
+setInterval(() => {
+    https.get(`${serverURL}/api/v1/ping`).on('error', (error) => {
+        console.error('Error pinging server:', error);
+    });
+}, 10 * 60 * 1000); // 10 minutes
