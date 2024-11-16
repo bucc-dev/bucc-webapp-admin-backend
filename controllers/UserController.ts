e81fa0c2cbdbfd3a5086e88ce8680806b3cbff14
tami-cp0 <findtamilore>
2024-11-16 22:51:47 +0100

    wip: user registration and verification

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -5,73 +5,216 @@ import IUser from '../interfaces/user';
 import { ErrorHandler } from '../middleware/errorHandler';
 import { config } from 'dotenv';
 import { CustomJwtPayload } from '../interfaces';
+import { sendVerificationMail } from '../utils/emails';
+import cache from '../utils/cache';
 
 config();
 
 class UserController {
-	static async login(req: Request, res: Response, next: NextFunction) {
+	static async register(req: Request, res: Response, next: NextFunction) {
+		const { firstname, lastname, email, password, role } = req.body;
+
+		const requiredFields = [
+			{ field: firstname, name: 'Firstname' },
+			{ field: lastname, name: 'Lastname' },
+			{ field: role, name: 'Role' },
+			{ field: email, name: 'Email' },
+			{ field: password, name: 'Password' },
+		];
+
+		for (const { field, name } of requiredFields) {
+			if (!field || field.trim() === '')
+				return next(new ErrorHandler(400, `${name} is missing`));
+
+			if (
+				name === 'Role' &&
+				!['senator', 'senate_president'].includes(field)
+			)
+				return next(new ErrorHandler(400, 'Invalid role'));
+		}
+
 		try {
-			const { email, password } = req.body;
+			const user = await User.findOne({ email });
+			if (!user) {
+				const newUser = await User.create({
+					firstname,
+					lastname,
+					email,
+					password,
+					role,
+				});
 
-			if (!email || !password)
-				throw new ErrorHandler(400, 'Email or Password is missing');
+				sendVerificationMail(newUser._id.toString(), email);
 
-			const user: IUser | null = await User.findOne({ email });
+				return res.status(201).json({
+					status: 'success',
+					message: 'An OTP has been sent to your email',
+				});
+			}
+			return next(new ErrorHandler(409, 'Email has already been used'));
+		} catch (error) {
+			return next(error);
+		}
+	}
 
-			if (!user || !(await user.isPasswordCorrect(password))) {
-                throw new ErrorHandler(401, 'Invalid email or password');
-            }
+	static async verifyOtp(req: Request, res: Response, next: NextFunction) {
+		const { otp, email } = req.body;
 
-            if (!user.isVerified) throw new ErrorHandler(403, 'Account is not verified');
+		if (!email) return next(new ErrorHandler(400, 'email is missing'));
+		if (!otp) return next(new ErrorHandler(400, 'otp is missing'));
 
-			if (user) {
-				const payload: CustomJwtPayload = { _id: user._id, email: user.email, accessLevel: user.accessLevel };
-				const accessToken: string = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '5min'});
-				const refreshToken: string = await user.generateRefreshToken(payload);
+		try {
+			const user = await User.findOne({ email });
+			if (!user)
+				return next(new ErrorHandler(404, 'User does not exist'));
 
-                res.cookie('accessToken', accessToken, { maxAge: 5 * 60 * 1000, httpOnly: true, secure: true });
-                res.cookie('refreshToken', refreshToken, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true }); // 7 days in ms
+			if (await cache.isOtpValid(user._id.toString(), otp)) {
+				user.isVerified = true;
 
-                res.status(200).json({
-                    status: 'success',
-                    message: 'Successfully logged in'
-                });
+				const payload: CustomJwtPayload = {
+					_id: user._id,
+					email: user.email,
+					accessLevel: user.accessLevel,
+				};
+				const accessToken: string = jwt.sign(
+					payload,
+					process.env.JWT_SECRET as string,
+					{ expiresIn: '5min' }
+				);
+				const refreshToken: string = await user.generateRefreshToken(
+					payload
+				);
+
+				res.cookie('accessToken', accessToken, {
+					maxAge: 5 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
+				});
+				res.cookie('refreshToken', refreshToken, {
+					maxAge: 7 * 24 * 60 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
+				}); // 7 days in ms
+
+				return res.status(200).json({
+					status: 'success',
+					message: 'Successfully logged in',
+				});
 			}
 		} catch (error) {
-			next(error);
+			return next(error);
+		}
+	}
+
+	static async resendOtp(req: Request, res: Response, next: NextFunction) {
+		const { email } = req.body;
+
+		if (!email) return next(new ErrorHandler(400, 'email is missing'));
+
+		try {
+			const user = await User.findOne({ email });
+			if (!user)
+				return next(new ErrorHandler(404, 'User does not exist'));
+
+			sendVerificationMail(user._id.toString(), email);
+
+			return res.status(200).json({
+				status: 'success',
+				message: 'OTP has been successfully sent to your email.',
+			});
+		} catch (error) {
+			return next(error);
+		}
+	}
+
+	static async login(req: Request, res: Response, next: NextFunction) {
+		const { email, password } = req.body;
+
+		if (!email || !password)
+			return next(new ErrorHandler(400, 'Email or Password is missing'));
+
+		try {
+			const user = await User.findOne({ email });
+
+			if (!user || !(await user.isPasswordCorrect(password))) {
+				return next(new ErrorHandler(401, 'Invalid email or password'));
+			}
+
+			if (!user.isVerified)
+				return next(new ErrorHandler(403, 'User is not verified'));
+
+			if (user) {
+				const payload: CustomJwtPayload = {
+					_id: user._id,
+					email: user.email,
+					accessLevel: user.accessLevel,
+				};
+				const accessToken: string = jwt.sign(
+					payload,
+					process.env.JWT_SECRET as string,
+					{ expiresIn: '5min' }
+				);
+				const refreshToken: string = await user.generateRefreshToken(
+					payload
+				);
+
+				res.cookie('accessToken', accessToken, {
+					maxAge: 5 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
+				});
+				res.cookie('refreshToken', refreshToken, {
+					maxAge: 7 * 24 * 60 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
+				}); // 7 days in ms
+
+				return res.status(200).json({
+					status: 'success',
+					message: 'Successfully logged in',
+				});
+			}
+		} catch (error) {
+			return next(error);
 		}
 	}
 
 	static async logout(req: Request, res: Response, next: NextFunction) {
 		try {
-			const user: IUser | null = await User.findById(req.user._id);
+			const user = await User.findById(req.user._id);
 
-			if (!user) throw new ErrorHandler(401, 'Login required');
+			if (!user) return next(new ErrorHandler(401, 'Login required'));
 
 			const refreshToken: string = req.cookies?.refreshToken;
 
 			if (user.refreshTokens.includes(refreshToken)) {
-				user.refreshTokens = user.refreshTokens.filter(rt => rt !== refreshToken);
+				user.refreshTokens = user.refreshTokens.filter(
+					(rt) => rt !== refreshToken
+				);
 				await user.save();
 			}
 
 			res.clearCookie('accessToken', { httpOnly: true, secure: true });
-            res.clearCookie('refreshToken', { httpOnly: true, secure: true });
+			res.clearCookie('refreshToken', { httpOnly: true, secure: true });
 
 			res.status(200).json({
 				status: 'success',
 				message: 'Logged out successfully',
 			});
 		} catch (error) {
-			next(error);
+			return next(error);
 		}
 	}
 
-	static async logoutAllDevices(req: Request, res: Response, next: NextFunction) {
+	static async logoutAllDevices(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
 		try {
-			const user: IUser | null = await User.findById(req.user._id);
+			const user = await User.findById(req.user._id);
 
-			if (!user) throw new ErrorHandler(401, 'Login required');
+			if (!user) return next(new ErrorHandler(401, 'Login required'));
 
 			const refreshToken: string = req.cookies?.refreshToken;
 
@@ -81,14 +224,14 @@ class UserController {
 			}
 
 			res.clearCookie('accessToken', { httpOnly: true, secure: true });
-            res.clearCookie('refreshToken', { httpOnly: true, secure: true });
+			res.clearCookie('refreshToken', { httpOnly: true, secure: true });
 
 			res.status(200).json({
 				status: 'success',
 				message: 'Logged out successfully',
 			});
 		} catch (error) {
-			next(error);
+			return next(error);
 		}
 	}
 }
