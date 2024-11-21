d2b64d5b59d50b30bc88da25323a1cac51874f34
tami-cp0 <findtamilore>
2024-11-22 00:26:09 +0100

    rename import

--- routes/user.ts
+++ routes/user.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import UserController from "../controllers/UserController";
+import userController from "../controllers/userController";
 import authMiddleware from "../middleware/authMiddleware";
 import { authLimiter, generalLimiter } from "../utils/limiters";
 
 const userRouter = Router();
 
-userRouter.route('/register').post(UserController.register);
-userRouter.route('/login').post(authLimiter, UserController.login);
-userRouter.route('/logout').post(authMiddleware, generalLimiter, UserController.logout);
-userRouter.route('/logout-all-devices').post(authMiddleware, generalLimiter, UserController.logoutAllDevices);
+userRouter.route('/register').post(userController.register);
+userRouter.route('/login').post(authLimiter, userController.login);
+userRouter.route('/logout').post(authMiddleware, generalLimiter, userController.logout);
+userRouter.route('/logout-all-devices').post(authMiddleware, generalLimiter, userController.logoutAllDevices);
 
 export default userRouter;
