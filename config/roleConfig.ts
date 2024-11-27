e9f8710c484a5a9b804f93a3be5cf627d23f0b14
tami-cp0 <findtamilore>
2024-11-27 20:39:15 +0100

    modified for own and others functionality

--- config/roleConfig.ts
+++ config/roleConfig.ts
@@ -1,31 +1,65 @@
-const defaultPermissions = {
+import { permissionAction, permissionResource } from '../interfaces/permission';
+
+type Role = 'admin' | 'super_admin';
+
+export const defaultPermissions: Record<
+	Role,
+	{ resource: permissionResource; actions: { own: permissionAction[], others: permissionAction[] } }[]
+> = {
 	admin: [
-		{
-			resource: 'announcements',
-			action: ['view', 'update', 'delete', 'create'],
-		},
-		{
-			resource: 'notifications',
-			action: ['view', 'update', 'delete', 'create'],
-		},
-		{ resource: 'users', action: ['view'] },
-		{ resource: 'course_materials', action: ['view'] },
-	],
-	super_admin: [
-		{
-			resource: 'announcements',
-			action: ['view', 'update', 'delete', 'create'],
-		},
-		{
-			resource: 'notifications',
-			action: ['view', 'update', 'delete', 'create'],
-		},
-		{ resource: 'users', action: ['view', 'create'] },
-		{
-			resource: 'course_materials',
-			action: ['view', 'update', 'delete', 'create'],
-		},
-	],
+        {
+            resource: 'announcements',
+            actions: {
+                own: ['view', 'update', 'delete', 'create'],
+                others: ['view'],
+            },
+        },
+        {
+            resource: 'notifications',
+            actions: {
+                own: ['view', 'update', 'delete', 'create'],
+                others: ['view'],
+            },
+        },
+        { resource: 'users', actions: { own: ['view', 'update', 'delete', 'create'], others: ['view'] } },
+        { resource: 'course_materials', actions: { own: ['view'], others: ['view'] } },
+    ],
+    super_admin: [
+        {
+            resource: 'announcements',
+            actions: {
+                own: ['view', 'update', 'delete', 'create'],
+                others: ['view', 'update', 'delete'],
+            },
+        },
+        {
+            resource: 'notifications',
+            actions: {
+                own: ['view', 'update', 'delete', 'create'],
+                others: ['view', 'update', 'delete'],
+            },
+        },
+        { resource: 'users', actions: { own: ['view', 'update', 'delete', 'create'], others: ['view', 'create'] } },
+        {
+            resource: 'course_materials',
+            actions: {
+                own: ['view', 'update', 'delete', 'create'],
+                others: ['view', 'update', 'delete'],
+            },
+        },
+    ]
 };
 
-export default defaultPermissions;
+export const allResourceActions: permissionAction[] = [
+	'view',
+	'update',
+	'delete',
+	'create',
+];
+
+export const allResources: permissionResource[] = [
+	'announcements',
+	'users',
+	'course_materials',
+	'notifications',
+];
