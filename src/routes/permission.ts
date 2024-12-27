c9c6d8f811a850a2b6c2be5019794156821dc341
tami-cp0 <findtamilore>
2024-12-27 22:18:59 +0100

    rename url paths

--- src/routes/permission.ts
+++ src/routes/permission.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import PermissionController from "../controllers/permissionController";
 import authMiddleware from "../middleware/authMiddleware";
-import { generalLimiter } from "../utils/limiters";
+import { minimalRateLimiter } from "../utils/limiters";
 
 const permissionRouter = Router();
 
-permissionRouter.route('/me/permissions/get').get(authMiddleware, generalLimiter, PermissionController.getPermission);
-permissionRouter.route('/me/permissions/check').get(authMiddleware, generalLimiter, PermissionController.checkPermission);
-permissionRouter.route('/:targetUserId/permissions/grant').patch(authMiddleware, generalLimiter, PermissionController.grantPermission);
-permissionRouter.route('/:targetUserId/permissions/revoke').patch(authMiddleware, generalLimiter, PermissionController.revokePermission);
+permissionRouter.route('/profile/permissions/get').get(authMiddleware, minimalRateLimiter, PermissionController.getPermission);
+permissionRouter.route('/profile/permissions/check').get(authMiddleware, minimalRateLimiter, PermissionController.checkPermission);
+permissionRouter.route('/:targetUserId/permissions/grant').patch(authMiddleware, minimalRateLimiter, PermissionController.grantPermission);
+permissionRouter.route('/:targetUserId/permissions/revoke').patch(authMiddleware, minimalRateLimiter, PermissionController.revokePermission);
 
 export default permissionRouter;
