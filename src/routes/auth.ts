3a1164bbb1c694e060c4492e2dcd2fe3da75152c
tami-cp0 <findtamilore>
2024-12-27 22:18:39 +0100

    add: auth routes

---
+++ src/routes/auth.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import authController from "../controllers/authController";
+import authMiddleware from "../middleware/authMiddleware";
+import { moderateRateLimiter, minimalRateLimiter, strictRateLimiter } from "../utils/limiters";
+
+const authRouter = Router();
+
+authRouter.route('/register').post(moderateRateLimiter, authController.register);
+authRouter.route('/login').post(moderateRateLimiter, authController.login);
+authRouter.route('/logout').post(authMiddleware, minimalRateLimiter, authController.logout);
+authRouter.route('/verify-account').post(authMiddleware, moderateRateLimiter, authController.verifyAccountToLogin);
+authRouter.route('/resend-account-verification-otp').post(strictRateLimiter, authController.resendOtp);
+authRouter.route('/refresh').post(minimalRateLimiter, authController.refreshAccessToken);
+
+export default authRouter;
