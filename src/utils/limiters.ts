10684dca0fbb1ecf8e10e1052d90973dfed04a9a
tami-cp0 <findtamilore>
2024-12-27 22:15:16 +0100

    add new limiter and rename

--- src/utils/limiters.ts
+++ src/utils/limiters.ts
@@ -1,44 +1,49 @@
 import rateLimit from "express-rate-limit";
 import { Response, Request, NextFunction } from "express";
 
-const rateLimitHandler = (req: Request, res: Response, next: NextFunction) => {
+const rateLimitHandler = (req: Request, res: Response, next: NextFunction, options: any) => {
+    const { time } = options;
     const url: string = req.originalUrl;
-    let message: string;
-    let time: string = '1 minute'; // default
-
-    if (url.includes('login')) {
-        message = 'login attempts';
-    } else if (url.includes('signup')) {
-        message = 'signup attempts';
-    } else {
-        message = 'requests';
-        time = '5 minutes';
-    }
 
     // wip
-    const logMessage = `Rate limit exceeded for IP: ${req.ip}, on URL: ${url}, with method: ${req.method}: too many ${message} with [` + `${req.body?.email ? `email: ${req.body.email}` : `ID: ${req?.user?._id}`}] \n`;
+    const logMessage = `Rate limit exceeded for IP: ${req.ip}, on URL: ${url}, with method: ${req.method}: too many requests with [` + `${req.body?.email ? `email: ${req.body.email}` : `ID: ${req?.user?._id}`}] \n`;
     console.log(logMessage);
 
     res.status(429).json({
         status: 'fail',
-        message: `Too many ${message}, please try again after ${time}`
+        message: `Too many requests, please try again after ${time}`
     });
 }
 
 /**
- * Limits requests to 5 per minute.
+ * Limits requests to 100 every 5 minutes.
  */
-export const authLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    max: 5,
-    handler: rateLimitHandler
+export const minimalRateLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+    handler: (req, res, next) => rateLimitHandler(req, res, next, {
+        time: "5 minutes"
+    })
 });
 
 /**
- * Limits requests to 100 every 5 minutes.
+ * Limits requests to 5 per minute.
  */
-export const generalLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 100,
-    handler: rateLimitHandler
+export const moderateRateLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 5,
+    handler: (req, res, next) => rateLimitHandler(req, res, next, {
+        time: "1 minute"
+    })
+});
+
+/**
+ * Limits requests to 1 per minute.
+ */
+export const strictRateLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 1,
+    handler: (req, res, next) => rateLimitHandler(req, res, next, {
+        time: "1 minute"
+    })
 });
