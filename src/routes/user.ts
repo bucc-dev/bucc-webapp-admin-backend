62e34a7e00b0cbd8a8178e45cc94112026e393db
tami-cp0 <findtamilore>
2024-12-27 22:19:15 +0100

    remove auth routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,15 +1,11 @@
 import { Router } from "express";
 import userController from "../controllers/userController";
 import authMiddleware from "../middleware/authMiddleware";
-import { authLimiter, generalLimiter } from "../utils/limiters";
+import { minimalRateLimiter, moderateRateLimiter } from "../utils/limiters";
 
 const userRouter = Router();
 
-userRouter.route('/register').post(userController.register);
-userRouter.route('/login').post(authLimiter, userController.login);
-userRouter.route('/logout').post(authMiddleware, generalLimiter, userController.logout);
-userRouter.route('/logout-all-devices').post(authMiddleware, generalLimiter, userController.logoutAllDevices);
-userRouter.route('/verify-otp').post(generalLimiter, userController.verifyOtp);
-userRouter.route('/resend-otp').post(generalLimiter, userController.resendOtp);
+userRouter.route('/profile').post();
+userRouter.route('/:targetUserId/profile').post();
 
 export default userRouter;
