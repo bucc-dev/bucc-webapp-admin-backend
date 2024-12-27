63e497810770fc3952dc6eac9854069d9d3e3d7b
tami-cp0 <findtamilore>
2024-12-27 22:15:48 +0100

    update: fix and move auth from user controller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,307 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { CustomJwtPayload } from '../interfaces';
+import { ErrorHandler } from '../utils/errorHandler';
+import User from '../models/users';
+import mongoose from 'mongoose';
+import { sendVerificationMail } from '../utils/emails';
+import cache from '../utils/cache';
+import IUser from '../interfaces/user';
+
+
+function isValidSchoolEmail(email: string) {
+    const pattern = /^[a-zA-Z0-9._%+-]+@(student|staff)\.babcock\.edu\.ng$/;
+    if (!pattern.test(email)) {
+        throw new ErrorHandler(400, "Please input a valid school email");
+    }
+}
+
+class authController {
+    /**
+     * Refreshes the access token using the provided refresh token.
+     * 
+     * @param {Request} req - The request object containing cookies and user information.
+     * @param {Response} res - The response object to send the new tokens.
+     * @param {NextFunction} next - The next middleware function.
+     * 
+     * @returns {Promise<void>} - Sends new access and refresh tokens as cookies or calls next with an error.
+     */
+	static async refreshAccessToken(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		const refreshToken: string | undefined = req.cookies?.refreshToken;
+
+		if (!refreshToken) return next(new ErrorHandler(401, 'Login required'));
+
+		const session = await mongoose.startSession();
+		session.startTransaction();
+
+		try {
+            const secret = process.env.JWT_SECRET as string;
+            const decoded: CustomJwtPayload = jwt.verify(refreshToken, secret) as CustomJwtPayload;
+
+            const user: IUser | null = await User.findById(decoded._id);
+            if (!user) {
+                return next(new ErrorHandler(401, "Login required - Invalid refresh token"));
+            }
+            
+			const payload: CustomJwtPayload = { _id: user._id };
+
+			if (!user.refreshTokens.includes(refreshToken)) {
+                return next(new ErrorHandler(401, "Login required - invalid refresh token"));
+            }
+
+			// generate refresh token func saves the document.
+			const newRefreshToken: string = await user.generateRefreshToken(
+				payload,
+                refreshToken
+			);
+
+			await session.commitTransaction();
+			session.endSession();
+
+            const newAccessToken: string = jwt.sign(
+				payload,
+				secret,
+				{
+					expiresIn: '50min',
+				}
+			);
+
+			res.cookie('accessToken', newAccessToken, {
+				maxAge: 5 * 60 * 1000,
+				httpOnly: true,
+				secure: true,
+				sameSite: true,
+			}); // 5 minutes
+			res.cookie('refreshToken', newRefreshToken, {
+				maxAge: 7 * 24 * 60 * 60 * 1000,
+				httpOnly: true,
+				secure: true,
+				sameSite: true,
+				path: '/auth/refresh',
+			}); // 7 days
+
+			return res.status(200).end();
+		} catch (error) {
+			await session.abortTransaction();
+			session.endSession();
+			return next(error);
+		}
+	}
+
+	static async register(req: Request, res: Response, next: NextFunction) {
+		const { firstname, lastname, email, password, role } = req.body;
+
+		const requiredFields = [
+			{ field: firstname, name: 'firstname' },
+			{ field: lastname, name: 'lastname' },
+			{ field: role, name: 'role' },
+			{ field: email, name: 'email' },
+			{ field: password, name: 'password' },
+		];
+
+		for (const { field, name } of requiredFields) {
+			if (!field || field.trim() === '')
+				return next(new ErrorHandler(400, `${name} is missing`));
+
+			if (name === 'role' && !['admin', 'super_admin'].includes(field))
+				return next(new ErrorHandler(400, 'Invalid role'));
+		}
+
+		try {
+            isValidSchoolEmail(email);
+
+			const user = await User.findOne({ email });
+			if (!user) {
+				const newUser = await User.create({
+					firstname,
+					lastname,
+					email,
+					password,
+					role,
+				});
+
+				sendVerificationMail(newUser._id.toString(), email);
+
+				return res.status(201).json({
+					status: 'success',
+					message: 'An OTP has been sent to your email',
+				});
+			}
+			return next(new ErrorHandler(409, 'Email has already been used'));
+		} catch (error) {
+			return next(error);
+		}
+	}
+
+	static async verifyAccountToLogin(req: Request, res: Response, next: NextFunction) {
+		const { otp, email } = req.body;
+
+		if (!email) return next(new ErrorHandler(400, 'email is missing'));
+		if (!otp) return next(new ErrorHandler(400, 'otp is missing'));
+
+		try {
+            isValidSchoolEmail(email);
+
+			const user = await User.findOne({ email });
+			if (!user)
+				return next(new ErrorHandler(404, 'Account does not exist'));
+
+			if (await cache.isOtpValid(user._id.toString(), otp)) {
+				if (user.isVerified == true) {
+					return res.status(201).json({
+						status: 'success',
+						message: 'Account is already verified',
+					});
+				}
+
+				user.isVerified = true;
+
+				const payload: CustomJwtPayload = {
+					_id: user._id,
+					email: user.email,
+					isVerified: user.isVerified,
+				};
+				const accessToken: string = jwt.sign(
+					payload,
+					process.env.JWT_SECRET as string,
+					{ expiresIn: '5min' }
+				);
+				const refreshToken: string = await user.generateRefreshToken(
+					payload
+				);
+
+				res.cookie('accessToken', accessToken, {
+					maxAge: 5 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
+                    sameSite: true
+				}); // 5min
+				res.cookie('refreshToken', refreshToken, {
+					maxAge: 7 * 24 * 60 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
+                    sameSite: true,
+                    path: '/auth/refresh'
+				}); // 7d
+
+				return res.status(200).json({
+					status: 'success',
+					message: 'Successfully logged in',
+				});
+			} else throw new ErrorHandler(401, 'otp is invalid');
+		} catch (error) {
+			return next(error);
+		}
+	}
+
+	static async resendOtp(req: Request, res: Response, next: NextFunction) {
+		const { email } = req.body;
+
+		if (!email) return next(new ErrorHandler(400, 'email is missing'));
+
+
+		try {
+            isValidSchoolEmail(email);
+
+			const user = await User.findOne({ email });
+			if (!user)
+				return next(new ErrorHandler(404, 'User does not exist'));
+
+			sendVerificationMail(user._id.toString(), email);
+
+			return res.status(200).json({
+				status: 'success',
+				message: 'OTP has been successfully sent to your email.',
+			});
+		} catch (error) {
+			return next(error);
+		}
+	}
+
+	static async login(req: Request, res: Response, next: NextFunction) {
+		const { email, password } = req.body;
+
+		if (!email) return next(new ErrorHandler(400, 'Email is missing'));
+		if (!password)
+			return next(new ErrorHandler(400, 'Password is missing'));
+
+		try {
+            isValidSchoolEmail(email);
+
+			const user = await User.findOne({ email });
+
+			if (!user || !(await user.isPasswordCorrect(password))) {
+				return next(new ErrorHandler(401, 'Invalid email or password'));
+			}
+
+			if (!user.isVerified)
+				return next(new ErrorHandler(403, 'Account is not verified'));
+
+			if (user) {
+				const payload: CustomJwtPayload = {
+					_id: user._id,
+					email: user.email,
+					isVerified: user.isVerified,
+				};
+				const accessToken: string = jwt.sign(
+					payload,
+					process.env.JWT_SECRET as string,
+					{ expiresIn: '50min' }
+				);
+				const refreshToken: string = await user.generateRefreshToken(
+					payload
+				);
+
+				res.cookie('accessToken', accessToken, {
+					maxAge: 5 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
+                    sameSite: true
+				}); // 5min
+				res.cookie('refreshToken', refreshToken, {
+					maxAge: 7 * 24 * 60 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
+                    sameSite: true,
+                    path: '/auth/refresh'
+				}); // 7d
+
+				return res.status(200).json({
+					status: 'success',
+					message: 'Successfully logged in',
+				});
+			}
+		} catch (error) {
+			return next(error);
+		}
+	}
+
+	static async logout(req: Request, res: Response, next: NextFunction) {
+        const accessToken: string = req.cookies?.accessToken;
+        
+		try {
+            await cache.blacklistAccessToken(req.user._id, accessToken);
+    
+			if (req.user.refreshTokens && req.user.refreshTokens.length > 0) {
+				req.user.refreshTokens = [];
+				await req.user.save();
+			}
+
+			res.clearCookie('accessToken', { httpOnly: true, secure: true, sameSite: true });
+			res.clearCookie('refreshToken', { httpOnly: true, secure: true, sameSite: true, path: '/auth/refresh' });
+
+			res.status(200).json({
+				status: 'success',
+				message: 'Logged out successfully',
+			});
+		} catch (error) {
+			return next(error);
+		}
+	}
+}
+
+export default authController;
