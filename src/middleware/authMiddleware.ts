84ed2a58187a71149de21efa61cc9400539c1842
tami-cp0 <findtamilore>
2024-12-27 22:17:47 +0100

    fix: auth flow to allow manual refreshing of tokens rather than automatic

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,26 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt, { TokenExpiredError } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import { CustomJwtPayload } from '../interfaces';
-import { ErrorHandler } from './errorHandler';
+import { ErrorHandler } from '../utils/errorHandler';
 import User from '../models/users';
 import IUser from '../interfaces/user';
-import mongoose from 'mongoose';
-
-// check isverified
+import cache from '../utils/cache';
 
 /**
  * Middleware to authenticate users using JWT tokens.
- * Verifies access token and refreshes it if expired.
- * Attaches user payload to request object.
- * Clears invalid tokens and handles errors.
+ * Verifies the access token and attaches the user payload to the request object.
+ * Handles errors and clears invalid tokens.
  * 
  * @param {Request} req - Express request object.
  * @param {Response} res - Express response object.
  * @param {NextFunction} next - Express next middleware function.
  */
+
 const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const accessToken: string | undefined = req.cookies?.accessToken;
-    const refreshToken: string | undefined = req.cookies?.refreshToken;
     const secret: string | undefined = process.env.JWT_SECRET;
 
     if (!accessToken || !secret) {
@@ -29,55 +26,21 @@ const authMiddleware = async (req: Request, res: Response, next: NextFunction) =
 
     try {
         const decoded: CustomJwtPayload = jwt.verify(accessToken, secret) as CustomJwtPayload;
-        req.user = decoded;
 
-        if (!req.user.isVerified) return next(new ErrorHandler(403, 'Account is not verified'));
-        return next();
-    } catch (error) {
-        if (error instanceof TokenExpiredError) {
-            if (!refreshToken) return next(new ErrorHandler(401, 'Login required'));
-
-            try {
-                jwt.verify(refreshToken, secret);
-            } catch (error) {
-                return next(new ErrorHandler(401, 'Login required'));
-            }
-
-            const user: IUser | null = await User.findOne({ refreshTokens: refreshToken });
-
-            if (user) {
-                const session = await mongoose.startSession();
-                session.startTransaction();
-
-                try {
-                    const payload: CustomJwtPayload = { _id: user._id, email: user.email, isVerified: user.isVerified };
-
-                    user.refreshTokens = user.refreshTokens.filter(rt => rt !== refreshToken);
-                    const newRefreshToken: string = await user.generateRefreshToken(payload);
-                    await session.commitTransaction();
-                    session.endSession();
-
-                    const newAccessToken: string = jwt.sign(payload, secret, { expiresIn: '5min' });
-
-                    res.cookie('accessToken', newAccessToken, { maxAge: 5 * 60 * 1000, httpOnly: true, secure: true }); // 5 minutes
-                    res.cookie('refreshToken', newRefreshToken, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true }); // 7 days
-
-                    req.user = payload;
-
-                    return next();
-                } catch (error) {
-                    await session.abortTransaction();
-                    session.endSession();
-                    return next(error);
-                }
-            }
+        if (await cache.isAccessTokenBlacklisted(decoded._id, accessToken)) {
+            return next(new ErrorHandler(401, "Login required"));
         }
 
-        // remove expired or invalid tokens
-        res.clearCookie('accessToken', { httpOnly: true, secure: true });
-        res.clearCookie('refreshToken', { httpOnly: true, secure: true });
+        const user: IUser | null = await User.findById(decoded._id);
+        if (!user) return next(new ErrorHandler(404, "Account does not exist: Invalid ID"));
+        if (!user.isVerified) return next(new ErrorHandler(403, 'Account is not verified'));
 
-        return next(new ErrorHandler(401, 'Login required'));
+        req.user = user;
+
+        return next();
+    } catch (error) {
+        console.log(error);
+        return next(new ErrorHandler(401, "Login required"));
     }
 };
 
