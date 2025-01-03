189197cc90c9c497edbd97da096ddf88ef3537ac
tami-cp0 <findtamilore>
2025-01-03 13:11:51 +0100

    add: getUser

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,42 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
 import User from '../models/users';
 import { ErrorHandler } from '../utils/errorHandler';
 import { config } from 'dotenv';
-import { CustomJwtPayload } from '../interfaces';
 import { sendVerificationMail } from '../utils/emails';
 import cache from '../utils/cache';
+import mongoose from 'mongoose';
+import IUser from '../interfaces/user';
+import { checkUserPermission } from '../utils/controllerUtils';
 
 config();
 
+
 class UserController {
+	static async getUser(req: Request, res: Response, next: NextFunction) {
+		let targetUserId: string | mongoose.Schema.Types.ObjectId = req.params.targetUserId;
+		let scope: 'own' | 'others' = targetUserId ? 'others' : 'own';
+
+		try {
+			await checkUserPermission(req.user, 'users', 'view', scope, targetUserId);
+
+			let targetUser: IUser | null;
+			if (targetUserId) {
+				targetUser = await User.findById(targetUserId);
+				if (!targetUser) {
+					return next(new ErrorHandler(404, "User does not exist"));
+				}
+			} else {
+				targetUser = req.user;
+			}
+
+			return res.status(200).json({
+				status: 'succcess',
+				data: targetUser.toJSON()
+			});
+		} catch (error) {
+			return next(error);
+		}
+	}
 	
 }
 
