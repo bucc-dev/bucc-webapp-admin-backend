eaf99aaef9d60ec70e1071d21b4da37136f2793f
tami-cp0 <findtamilore>
2025-01-11 22:05:29 +0100

    complete routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,20 +1,27 @@
 import { Router } from "express";
 import userController from "../controllers/userController";
 import authMiddleware from "../middleware/authMiddleware";
-import { minimalRateLimiter, moderateRateLimiter } from "../utils/limiters";
+import { minimalRateLimiter, moderateRateLimiter, strictRateLimiter } from "../utils/limiters";
 import authController from "../controllers/authController";
 
 const userRouter = Router();
 
-userRouter.route(['/self/profile', '/:targetUserId/profile']).get();
-userRouter.route(['/self/profile', '/:targetUserId/profile']).delete();
-userRouter.route(['/self/profile/update/role', '/:targetUserId/profile/update/role']).patch();
-userRouter.route('/self/profile/update/name').patch();
-userRouter.route('/self/profile/update/password').patch();
-userRouter.route('/self/profile/update/email').post();
-userRouter.route('/self/profile/update/set-new-email').patch();
-userRouter.route('/self/profile/update/email/resendOtp').post(authMiddleware, authController.resendOtp);
-userRouter.route('/self/profile/forgot-password').post();
-userRouter.route('/self/profile/set-new-password').patch();
+userRouter.route(['/self/profile', '/:targetUserId/profile']).get(authMiddleware, minimalRateLimiter, userController.getUser);
+
+userRouter.route(['/self/profile/delete', '/:targetUserId/profile/delete']).delete(authMiddleware, minimalRateLimiter, userController.deleteUser);
+
+userRouter.route('/:targetUserId/profile/update/role').patch(authMiddleware, moderateRateLimiter, userController.updateUserRole);
+
+userRouter.route('/self/profile/update/name').patch(authMiddleware, moderateRateLimiter, userController.updateUserName);
+
+userRouter.route('/self/profile/update/password').patch(authMiddleware, moderateRateLimiter, userController.updateUserPassword);
+
+userRouter.route(['/self/profile/forgot-password', '/self/profile/forgot-password/resend-verification-link']).post(strictRateLimiter, userController.forgotPassword);
+
+userRouter.route('/self/profile/reset-password').patch(authMiddleware, moderateRateLimiter, userController.resetPassword);
+
+userRouter.route(['/self/profile/update/email', '/self/profile/update/email/resend-otp']).post(authMiddleware, strictRateLimiter, userController.updateEmail);
+
+userRouter.route('/self/profile/update/set-new-email').patch(moderateRateLimiter, userController.setNewEmail);
 
 export default userRouter;
