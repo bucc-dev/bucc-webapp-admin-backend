d59e7eefcf0ca1d63d67a88a625cdbacefdb707c
tami-cp0 <findtamilore>
2025-01-11 22:03:37 +0100

    add reset link email

--- src/utils/emails.ts
+++ src/utils/emails.ts
@@ -1,9 +1,10 @@
 import nodemailer from 'nodemailer';
 import cache from './cache.js';
 import { ErrorHandler } from './errorHandler.js';
+import IUser from '../interfaces/user.js';
 
 /**
- * Sends a verification email with an OTP to the specified user.
+ * Generates OTP and Sends a verification email with the OTP to the specified user.
  * 
  * @param {string} userId - The ID of the user.
  * @param {string} email - The email address of the user.
@@ -36,3 +37,26 @@ export async function sendVerificationMail(userId: string, email: string): Promi
 		return new ErrorHandler(500, 'Request a new email');
 	}
 }
+
+
+export async function sendPasswordResetEmail(user: IUser, accessToken: string) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: user.email,
+    subject: 'Password Reset',
+    html: `<p>Click the link below to reset your password:
+
+			${process.env.FRONTEND_URL}/reset-password?token=${accessToken}</p>
+	
+			<p>If you didn't request this, contact support.</p>
+	        <h4>DO NOT REPLY TO THIS AUTOMATED EMAIL.</h4>`,
+  });
+};
