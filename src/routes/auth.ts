aed53061e504122fcfa9b226f89988f49c77b5fd
tami-cp0 <findtamilore>
2025-01-11 22:05:10 +0100

    add spacing

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,10 +6,15 @@ import { moderateRateLimiter, minimalRateLimiter, strictRateLimiter } from "../u
 const authRouter = Router();
 
 authRouter.route('/register').post(moderateRateLimiter, authController.register);
+
 authRouter.route('/login').post(moderateRateLimiter, authController.login);
+
 authRouter.route('/logout').post(authMiddleware, minimalRateLimiter, authController.logout);
+
 authRouter.route('/verify-account').post(authMiddleware, moderateRateLimiter, authController.verifyAccountToLogin);
+
 authRouter.route('/resend-account-verification-otp').post(strictRateLimiter, authController.resendOtp);
+
 authRouter.route('/refresh').post(minimalRateLimiter, authController.refreshAccessToken);
 
 export default authRouter;
