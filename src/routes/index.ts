f978c7e56471e002bb30907e996b0dfc41dda887
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-01-16 12:48:46 +0100

    refactor: improve formatting and consistency in courseMaterial model and routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,28 +6,29 @@ import permissionRouter from "./permission";
 import cache from "../utils/cache";
 import courseMaterialRouter from "./courseMaterial";
 
-
 const router: Router = Router();
 
-router.use('/api/v1/users', userRouter);
-router.use('/api/v1/users', permissionRouter);
-router.use('/api/v1/auth', authRouter);
-router.use('/api/v1/course-materials', courseMaterialRouter);
+router.use("/api/v1/users", userRouter);
+router.use("/api/v1/users", permissionRouter);
+router.use("/api/v1/auth", authRouter);
+router.use("/api/v1/course-materials", courseMaterialRouter);
 
-router.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+router.use(
+  (err: CustomError, req: Request, res: Response, next: NextFunction) => {
     handleError(err, res);
+  }
+);
+
+router.all("*", (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Cannot ${req.method} ${req.originalUrl}`,
+  });
 });
 
-router.all('*', (req: Request, res: Response, next: NextFunction) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Cannot ${req.method} ${req.originalUrl}`
-    });
-});
-
-router.use('/api/v1/ping', (req, res) => {
-    cache.connected
-    return res.status(200).end();
+router.use("/api/v1/ping", (req, res) => {
+  cache.connected;
+  return res.status(200).end();
 });
 
 export default router;
