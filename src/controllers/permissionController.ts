6535c90b65406d41ba19aa10781dd2a57dfb2c1e
tami-cp0 <findtamilore>
2025-02-08 00:23:14 +0100

    refactor permission controller

--- src/controllers/permissionController.ts
+++ src/controllers/permissionController.ts
@@ -4,154 +4,183 @@ import Permission from '../models/permissions';
 import { ErrorHandler } from '../utils/errorHandler';
 import { IPermission } from '../interfaces/permission';
 
-
 class PermissionController {
-    static async getPermission(req: Request, res: Response, next: NextFunction) {
-        try {
-            const user = await User.findById(req.user._id);
-            if (!user) 
-                throw new ErrorHandler(404, 'User does not exist');
+	static async getPermission(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		const { targetUserId } = req.params;
+        if (!targetUserId) {
+            return next(new ErrorHandler(400, 'targetUserId is missing'));
+        }
 
-            let permissionDocument = (await Permission.findOne({
-                userId: user._id,
-            })) as IPermission;
-            if (!permissionDocument) {
-                permissionDocument = await Permission.create({
-                    userId: user._id,
-                    role: user.role,
-                });
+		try {
+            // students can't view permission and admins can only view their permission
+			if (
+				req.user.role === 'student' ||
+				(req.user._id.toString() !== targetUserId &&
+					req.user.role !== 'super_admin')
+			) return next(new ErrorHandler(403, 'Access denied'));
+
+            let targetUser;
+            if (req.user._id.toString() === targetUserId ) {
+                targetUser = req.user;
+            } else {
+			    targetUser = await User.findById(targetUserId);
+			    if (!targetUser) return next(new ErrorHandler(404, 'User does not exist'));
             }
 
-            return res.status(200).json({
-                status: 'success',
-                data: permissionDocument
-            })
-        } catch (error) {
-            return next(error);
-        } 
-    }
+			const permissionDocument = (await Permission.findOne({
+				userId: targetUser._id,
+			})) as IPermission;
 
-	static async checkPermission(req: Request, res: Response, next: NextFunction) {
-        try {
-            const user = await User.findById(req.user._id);
-            if (!user) 
-                throw new ErrorHandler(404, 'User does not exist');
+			return res.status(200).json({
+				status: 'success',
+				data: permissionDocument,
+			});
+		} catch (error) {
+			return next(error);
+		}
+	}
 
-            const { resource, action } = req.body;
-            const scope: 'own' | 'others' = req.body.scope;
+	static async checkPermission(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) { 
+		try {
+			const { resource, action } = req.body;
+			const scope: 'own' | 'others' = req.body.scope;
 
-            const requiredFields = [
-                { field: resource, name: 'resource' },
-                { field: action, name: 'action' },
-                { field: scope, name: 'scope' }
-            ];
+			const requiredFields = [
+				{ field: resource, name: 'resource' },
+				{ field: action, name: 'action' },
+				{ field: scope, name: 'scope' },
+			];
 
-            for (const { field, name } of requiredFields) {
-                if (!field || field.trim() === '')
-                    return next(new ErrorHandler(400, `${name} is missing`));
-            }
+			for (const { field, name } of requiredFields) {
+				if (!field || field.trim() === '')
+					return next(new ErrorHandler(400, `${name} is missing`));
+			}
 
-            let hasPermission: boolean = true;
-            let message: string = 'User has permission';
-            let statusCode: number = 200;
+			let hasPermission: boolean = true;
+			let message: string = 'User has permission';
+			let statusCode: number = 200;
 
-            if(!(await user.hasPermission(resource, action, scope))) {
-                hasPermission = false;
-                message = 'User does not have permission';
-                statusCode = 403;
-            }
+			if (!(await req.user.hasPermission(resource, action, scope))) {
+				hasPermission = false;
+				message = 'User does not have permission';
+				statusCode = 403;
+			}
 
-            return res.status(statusCode).json({
-                status: 'success',
-                message,
-                hasPermission
-            })
-        } catch (error) {
-            return next(error);
-        } 
-    }
+			return res.status(statusCode).json({
+				status: 'success',
+				message,
+				hasPermission,
+			});
+		} catch (error) {
+			return next(error);
+		}
+	}
 
-    static async grantPermission(req: Request, res: Response, next: NextFunction) {
-        try {
-            const currentUser = await User.findById(req.user._id);
-            if (!currentUser) 
-                throw new ErrorHandler(404, 'User does not exist');
+	static async grantPermission(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		try {
+			const currentUser = req.user
 
-            const targetUserId = req.params.targetUserId;
-            const { resource, action, scope } = req.body;
-            
-            const requiredFields = [
-                { field: targetUserId, name: 'targetUserId' },
-                { field: resource, name: 'resource' },
-                { field: action, name: 'action' },
-                { field: scope, name: 'scope' },
-            ];
-    
-            for (const { field, name } of requiredFields) {
-                if (!field || field.trim() === '')
-                    return next(new ErrorHandler(400, `${name} is missing`));
-            }
+			const targetUserId = req.params.targetUserId;
+			const { resource, action, scope } = req.body;
 
-            const targetUser = await User.findById(targetUserId);
-            if (!targetUser)
-                throw new ErrorHandler(404, 'Target user does not exist');
+			const requiredFields = [
+				{ field: targetUserId, name: 'targetUserId' },
+				{ field: resource, name: 'resource' },
+				{ field: action, name: 'action' },
+				{ field: scope, name: 'scope' },
+			];
 
-            let message: string = 'Permission granted';
+			for (const { field, name } of requiredFields) {
+				if (!field || field.trim() === '')
+					return next(new ErrorHandler(400, `${name} is missing`));
+			}
 
-            const messageResponse = await Permission.grantPermission(currentUser, targetUser, resource, action, scope);
-            if (messageResponse) {
-                message = messageResponse;
-            }
+			const targetUser = await User.findById(targetUserId);
+			if (!targetUser)
+				throw new ErrorHandler(404, 'Target user does not exist');
 
-            return res.status(200).json({
-                status: 'success',
-                message
-            });
-        } catch (error) {
-            return next(error);
-        } 
-    }
+			let message: string = 'Permission granted';
 
-    static async revokePermission(req: Request, res: Response, next: NextFunction) {
-        try {
-            const currentUser = await User.findById(req.user._id);
-            if (!currentUser) 
-                throw new ErrorHandler(404, 'User does not exist');
+			const messageResponse = await Permission.grantPermission(
+				currentUser,
+				targetUser,
+				resource,
+				action,
+				scope
+			);
+			if (messageResponse) {
+				message = messageResponse;
+			}
 
-            const targetUserId = req.params.targetUserId;
-            const { resource, action, scope } = req.body;
-            
-            const requiredFields = [
-                { field: targetUserId, name: 'targetUserId' },
-                { field: resource, name: 'resource' },
-                { field: action, name: 'action' },
-                { field: scope, name: 'scope' },
-            ];
-    
-            for (const { field, name } of requiredFields) {
-                if (!field || field.trim() === '')
-                    return next(new ErrorHandler(400, `${name} is missing`));
-            }
+			return res.status(200).json({
+				status: 'success',
+				message,
+			});
+		} catch (error) {
+			return next(error);
+		}
+	}
 
-            const targetUser = await User.findById(targetUserId);
-            if (!targetUser)
-                throw new ErrorHandler(404, 'Target user does not exist');
+	static async revokePermission(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		try {
+			const currentUser = req.user;
 
-            let message: string = 'Permission revoked';
+			const targetUserId = req.params.targetUserId;
+			const { resource, action, scope } = req.body;
 
-            const messageResponse = await Permission.revokePermission(currentUser, targetUser, resource, action, scope);
-            if (messageResponse) {
-                message = messageResponse;
-            }
+			const requiredFields = [
+				{ field: targetUserId, name: 'targetUserId' },
+				{ field: resource, name: 'resource' },
+				{ field: action, name: 'action' },
+				{ field: scope, name: 'scope' },
+			];
 
-            return res.status(200).json({
-                status: 'success',
-                message
-            });
-        } catch (error) {
-            return next(error);
-        } 
-    }
+			for (const { field, name } of requiredFields) {
+				if (!field || field.trim() === '')
+					return next(new ErrorHandler(400, `${name} is missing`));
+			}
+
+			const targetUser = await User.findById(targetUserId);
+			if (!targetUser)
+				throw new ErrorHandler(404, 'Target user does not exist');
+
+			let message: string = 'Permission revoked';
+
+			const messageResponse = await Permission.revokePermission(
+				currentUser,
+				targetUser,
+				resource,
+				action,
+				scope
+			);
+			if (messageResponse) {
+				message = messageResponse;
+			}
+
+			return res.status(200).json({
+				status: 'success',
+				message,
+			});
+		} catch (error) {
+			return next(error);
+		}
+	}
 }
 
 export default PermissionController;
