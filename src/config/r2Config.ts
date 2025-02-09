a666b1a9d31900e08829d2c983f5a17423feff37
tami-cp0 <findtamilore>
2025-02-09 13:34:02 +0100

    add file filter for pre-upload validation

--- src/config/r2Config.ts
+++ src/config/r2Config.ts
@@ -1,9 +1,6 @@
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-import multerS3 from "multer-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import multer, { Multer } from "multer";
 import dotenv from "dotenv";
-
 import { ErrorHandler } from "../utils/errorHandler";
 
 dotenv.config();
@@ -25,29 +22,10 @@ export const r2Client = new S3Client({
   },
 });
 
+export type ValidBuckets = 'announcementmedia';
+export const validBuckets: ValidBuckets[] = ['announcementmedia'];
 
-
-const s3Storage = multerS3({
-  s3: r2Client,
-  bucket: (req: any, file: Express.Multer.File, cb: (error: any, bucket?: string | undefined) => void) => {
-    const bucketName = req.body.bucketName;
-    if (!bucketName) {
-      return cb(new ErrorHandler(400, "Bucket name not provided"), undefined);
-    }
-    cb(null, bucketName);
-  },
-  acl: "public-read", // Files are private; they require signed URLs for access.
-  contentType: multerS3.AUTO_CONTENT_TYPE,
-  key: (req: Express.Request, file: Express.Multer.File, cb: (error: any, key?: string) => void) => {
-    const key = `${Date.now()}-${file.originalname}`;
-    cb(null, key);
-  },
-});
-
-export const upload = multer({ storage: s3Storage });
-
-
-export async function getSignedUrlForFile(bucketName: 'announcementmedia', key: string) {
+export async function getSignedUrlForFile(bucketName: ValidBuckets, key: string) {
   const command = new GetObjectCommand({
     Bucket: bucketName,
     Key: key,
