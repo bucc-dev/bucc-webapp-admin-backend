82ed01ebb4fbb966f0b8a2bf3bb944f95d6bd702
tami-cp0 <findtamilore>
2025-02-09 12:15:30 +0100

    fix routes

--- src/routes/permission.ts
+++ src/routes/permission.ts
@@ -5,12 +5,12 @@ import { minimalRateLimiter } from "../utils/limiters";
 
 const permissionRouter = Router();
 
-permissionRouter.route('/:targetUserId/get').get(authMiddleware, minimalRateLimiter, PermissionController.getPermission);
+permissionRouter.route('/:targetUserId/permissions/get').get(authMiddleware, minimalRateLimiter, PermissionController.getPermission);
 
-permissionRouter.route('/self/check').get(authMiddleware, minimalRateLimiter, PermissionController.checkPermission);
+permissionRouter.route('/self/permission/check').get(authMiddleware, minimalRateLimiter, PermissionController.checkPermission);
 
-permissionRouter.route('/:targetUserId/grant').patch(authMiddleware, minimalRateLimiter, PermissionController.grantPermission);
+permissionRouter.route('/:targetUserId/permission/grant').patch(authMiddleware, minimalRateLimiter, PermissionController.grantPermission);
 
-permissionRouter.route('/:targetUserId/revoke').patch(authMiddleware, minimalRateLimiter, PermissionController.revokePermission);
+permissionRouter.route('/:targetUserId/permission/revoke').patch(authMiddleware, minimalRateLimiter, PermissionController.revokePermission);
 
 export default permissionRouter;
