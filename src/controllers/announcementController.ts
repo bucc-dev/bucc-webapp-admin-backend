8cd7d4362860ff2343b12538aaa29b1e97192a54
tami-cp0 <findtamilore>
2025-02-10 11:05:18 +0100

    add delete and paginated get

--- src/controllers/announcementController.ts
+++ src/controllers/announcementController.ts
@@ -4,8 +4,9 @@ import Announcement from '../models/announcements';
 import { checkUserPermission } from '../utils/checkPermission';
 import IUser from '../interfaces/user';
 import { IAnnouncement, IAnnouncementMedia } from '../interfaces/announcement';
-import { getSignedUrlForFile, r2Client } from '../config/r2Config';
+import { deleteBucketObject, getSignedUrlForFile, r2Client } from '../config/r2Config';
 import { IMulterS3File } from '../interfaces';
+import mongoose from 'mongoose';
 
   
 class announcementController {
@@ -69,10 +70,22 @@ class announcementController {
             return next(new ErrorHandler(400, 'announcementId is missing'));
         }
 
+        const targetUserId = req.params?.targetUserId;
+
         try {
             await checkUserPermission(req.user, 'announcements', 'read')
 
-            const announcement = await Announcement.findById(announcementId).populate({ path: 'owner', select: 'role firstname _id lastname' }).lean();
+            let query: { _id: string | mongoose.Schema.Types.ObjectId, owner?: string | mongoose.Schema.Types.ObjectId };
+            if (targetUserId) {
+                query = {
+                    _id: announcementId,
+                    owner: targetUserId
+                }
+            } else {
+                query = { _id: announcementId };
+            }
+
+            const announcement = await Announcement.findOne(query).populate({ path: 'owner', select: 'role firstname _id lastname' }).lean();
 
             if (!announcement) {
                 return next(new ErrorHandler(400, 'Invalid announcementId'));
@@ -133,34 +146,128 @@ class announcementController {
         }
     }
 
-    // static async deleteAnnouncement(
-	// 	req: Request,
-	// 	res: Response,
-	// 	next: NextFunction
-	// ) {
-    //     const { announcementId } = req.params;
-    //     if (!announcementId) {
-    //         return next(new ErrorHandler(400, 'announcementId is missing'));
-    //     }
+    static async deleteAnnouncement(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+        const { announcementId } = req.params;
+        if (!announcementId) {
+            return next(new ErrorHandler(400, 'announcementId is missing'));
+        }
 
-    //     try {
-    //         const announcement = await Announcement.findByIdAndDelete(announcementId)
-    //         if (!announcement) {
-    //             return next(new ErrorHandler(400, 'Invalid announcementId'));
-    //         }
+        try {
+            const announcement = await Announcement.findById(announcementId);
+            if (!announcement) {
+                return next(new ErrorHandler(400, 'Invalid announcementId'));
+            }
 
-    //         const owner: IUser = announcement.owner as IUser;
-    //         await checkUserPermission(req.user, 'announcements', 'update', owner._id);
+            const owner = announcement.owner as mongoose.Schema.Types.ObjectId;
+            await checkUserPermission(req.user, 'announcements', 'delete', owner);
 
-    //         return res.status(200).json({
-    //             status: 'success',
-    //             message: 'caption has been updated',
-    //             data: announcement
-    //         })
-    //     } catch (error) {
-    //         return next(error);
-    //     }
-    // }
+            await Promise.all(
+                announcement.media.map(async (media) => {
+                    await deleteBucketObject(media.bucketName, media.key);
+                })
+            );
+
+            await announcement.deleteOne();
+
+            return res.sendStatus(204);
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    static async paginatedGet(req: Request, res: Response, next: NextFunction) {
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+    
+        const targetUserId = req.params?.targetUserId;
+    
+        try {
+            // Build the aggregation pipeline dynamically.
+            const pipeline: any[] = [];
+    
+            // If a targetUserId is provided, add a $match stage at the beginning of the pipeline
+            if (targetUserId) {
+                pipeline.push({
+                    $match: {
+                        owner: new mongoose.Types.ObjectId(targetUserId)
+                    }
+                });
+            }
+    
+            pipeline.push(
+                {
+                    $sort: {
+                        updatedAt: -1,
+                    },
+                },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner",
+                    },
+                },
+                {
+                    $unwind: {
+                        path: "$owner"
+                    },
+                },
+                {
+                    $project: {
+                        "owner.refreshTokens": 0,
+                        "owner.email": 0,
+                        "owner.password": 0
+                    }
+                },
+                {
+                    $facet: {
+                        metadata: [{ $count: "totalAnnouncements" }],
+                        announcements: [{ $skip: skip }, { $limit: limit }],
+                    },
+                }
+            );
+    
+            const result = await Announcement.aggregate(pipeline);
+    
+            // Extract announcements from the aggregation result
+            const announcements = result[0]?.announcements || [];
+    
+            // Process each announcement's media field to add signed URLs in parallel
+            await Promise.all(
+                announcements.map(async (announcement: IAnnouncement) => {
+                    announcement.media = await Promise.all(
+                        announcement.media.map(async (media) => ({
+                            ...media,
+                            signedUrl: await getSignedUrlForFile(media.bucketName, media.key),
+                        }))
+                    );
+                })
+            );
+    
+            const totalAnnouncements = result[0]?.metadata[0]?.totalAnnouncements ?? 0;
+    
+            return res.status(200).json({
+                status: "success",
+                data: {
+                    metadata: {
+                        page,
+                        limit,
+                        totalAnnouncements,
+                        totalPages: Math.ceil(totalAnnouncements / limit)
+                    },
+                    announcements,
+                },
+            });
+        } catch (error) {
+            return next(error);
+        }
+    }      
 }
 
 export default announcementController;
