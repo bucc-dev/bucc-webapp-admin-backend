23f4cf0bf624190aeb742b3cf772defcb0e9702d
tami-cp0 <findtamilore>
2025-02-10 22:35:02 +0100

    rename routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import userController from "../controllers/userController";
 import authMiddleware from "../middleware/authMiddleware";
 import { minimalRateLimiter, moderateRateLimiter, strictRateLimiter } from "../utils/limiters";
+import announcementController from "../controllers/announcementController";
 
 const userRouter = Router();
 
-userRouter.route(['/self/get', '/:targetUserId/get']).get(authMiddleware, minimalRateLimiter, userController.getUser);
+userRouter.route('/:targetUserId').get(authMiddleware, minimalRateLimiter, userController.getUser);
 
-userRouter.route(['/self/delete', '/:targetUserId/delete']).delete(authMiddleware, minimalRateLimiter, userController.deleteUser);
+userRouter.route('/:targetUserId').delete(authMiddleware, minimalRateLimiter, userController.deleteUser);
 
 userRouter.route('/:targetUserId/update/role').patch(authMiddleware, moderateRateLimiter, userController.updateUserRole);
 
@@ -15,14 +16,16 @@ userRouter.route('/self/update/name').patch(authMiddleware, moderateRateLimiter,
 
 userRouter.route('/self/update/password').patch(authMiddleware, moderateRateLimiter, userController.updateUserPassword);
 
-userRouter.route(['/self/forgot-password', '/self/forgot-password/resend-verification-link']).post(strictRateLimiter, userController.forgotPassword);
+userRouter.route('/forgot-password').post(strictRateLimiter, userController.forgotPassword);
 
-userRouter.route('/self/reset-password').patch(authMiddleware, moderateRateLimiter, userController.resetPassword);
+userRouter.route('/reset-password').patch(authMiddleware, moderateRateLimiter, userController.resetPassword);
 
-userRouter.route(['/self/update/email', '/self/update/email/resend-otp']).post(authMiddleware, strictRateLimiter, userController.updateEmail);
+userRouter.route('/self/update/email-otp').post(authMiddleware, strictRateLimiter, userController.updateEmail);
 
-userRouter.route('/self/update/set-new-email').patch(moderateRateLimiter, userController.setNewEmail);
+userRouter.route('/self/update/set-new-email').patch(authMiddleware, moderateRateLimiter, userController.setNewEmail);
 
-// userRouter.route(['/self/announcements/paginated-get', '/:targetUserId/announcements/paginated-get']).get(authMiddleware, moderateRateLimiter);
+userRouter.route('/:targetUserId/announcements').get(authMiddleware, moderateRateLimiter, announcementController.paginatedGet);
+
+userRouter.route('/:targetUserId/announcements/:announcementId').get(authMiddleware, moderateRateLimiter, announcementController.getAnnouncement);
 
 export default userRouter;
