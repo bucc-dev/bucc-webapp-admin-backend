3a6921b0c566abe617e838ea25e5cf9b761b210b
tami-cp0 <findtamilore>
2025-02-15 19:55:26 +0100

    update route limiters

--- src/routes/announcement.ts
+++ src/routes/announcement.ts
@@ -6,14 +6,14 @@ import { upload } from "../middleware/upload";
 
 const announcementRouter = Router();
 
-announcementRouter.route('/:announcementId').get(moderateRateLimiter, authMiddleware, announcementController.getAnnouncement);
+announcementRouter.route('/:announcementId').get(minimalRateLimiter, authMiddleware, announcementController.getAnnouncement);
 
-announcementRouter.route('/').post(moderateRateLimiter, authMiddleware, upload.array('file', 3), announcementController.postAnnouncement);
+announcementRouter.route('/').post(moderateRateLimiter, authMiddleware, upload.array('files', 3), announcementController.postAnnouncement);
 
 announcementRouter.route('/:announcementId/update-caption').patch(moderateRateLimiter, authMiddleware, announcementController.updateAnnouncementCaption);
 
-announcementRouter.route('/:announcementId').delete(moderateRateLimiter, authMiddleware, announcementController.deleteAnnouncement);
+announcementRouter.route('/:announcementId').delete(minimalRateLimiter, authMiddleware, announcementController.deleteAnnouncement);
 
-announcementRouter.route('/').get(moderateRateLimiter, authMiddleware, announcementController.paginatedGet);
+announcementRouter.route('/').get(minimalRateLimiter, authMiddleware, announcementController.paginatedGet);
 
 export default announcementRouter;
