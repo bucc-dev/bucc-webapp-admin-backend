88fce703a01f67491e94138c39ea3f58f078bed2
tami-cp0 <findtamilore>
2025-02-15 15:21:19 +0100

    auto generate model schemas and add extra notes

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,4 +1,14 @@
 import swaggerJsdoc from "swagger-jsdoc";
+import m2s from "mongoose-to-swagger";
+import Announcement from "../models/announcements";
+import User from "../models/users";
+import Permission from "../models/permissions";
+
+const swaggerSchemas = {
+  User: m2s(User),
+  Announcement: m2s(Announcement),
+  Permission: m2s(Permission),
+};
 
 const options: swaggerJsdoc.Options = {
   definition: {
@@ -6,10 +16,22 @@ const options: swaggerJsdoc.Options = {
     info: {
       title: "BUCC WebApp",
       version: "1.0.0",
-      description: "API documentation",
+      description: `
+API documentation
+
+**Extra Notes:**
+- There's no need to manually set and authorize as the tokens are cookies so the browser receives and sends automatically
+- View the external doc/README for valid resources, scopes and actions.
+      `,
       contact: {
-        email: "findtamilore@gmail.com"
+        name: "Contact the Developer: linkedIn",
+        url: "https://www.linkedin.com/in/tami-cp0"
       }
+    },
+
+    externalDocs: {
+      description: "README for developer information",
+      url: "https://github.com/bucc-dev/bucc-webapp-admin-backend"
     },
     servers: [
       {
@@ -17,9 +39,14 @@ const options: swaggerJsdoc.Options = {
         description: "Local server",
       },
     ],
+    components: {
+      schemas: swaggerSchemas,
+    },
   },
   apis: ["./src/docs/**/*.ts"],
 };
+
+
 
 const swaggerSpec = swaggerJsdoc(options);
 
